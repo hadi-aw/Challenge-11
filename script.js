8727155ca0810ca9db610b082ec4f8a803fab825
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-06-20 12:05:05 -0400

    script.js
    "Configure linear scale for bar widths."

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const svgHeight = data.length * (20 + 1);
 const svg = d3.select('body').append('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
+
+    // Linear scale for bar widths
+    const xScale = d3.scaleLinear()
+    .domain([d3.min(data), d3.max(data)])
+    .range([50, svgWidth]);
