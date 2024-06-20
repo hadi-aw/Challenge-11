4b4719dffe5ed1d07832caab2a3b5d0a0bd9522d
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-06-20 12:08:31 -0400

    script.js
    "Render bars and group elements based on scaled data."

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ const svg = d3.select('body').append('svg')
     const xScale = d3.scaleLinear()
     .domain([d3.min(data), d3.max(data)])
     .range([50, svgWidth]);
+
+    // Render bars and groups elements based on scaled data
+    svg.selectAll('g')
+    .data(data)
+    .enter().append('g')
+    .attr('transform', (d, i) => `translate(0, ${i * (20 + 1)})`)
+    .append('rect')
+    .attr('width', d => xScale(d))
+    .attr('height', 20);
